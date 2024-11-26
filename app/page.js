f807e2f380484bfe0b7c3f746dbffb87fca286b5
Keremimo <nyaa@live.com>
2024-11-26 12:08:52 +0100

    Add greeting, modify main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import Image from "next/image";
-
+'use client';
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+import { useState } from "react";
 export default function Home() {
+  const [count, setCount] = useState(0)
   return (
     <div>
       <h1 className="font-bold">Main Page</h1>
+      <p>{count}</p>
+      <Button onClick={() => setCount(count + 1)}>Click me
+      </Button>
     </div>
   );
 }
