6e9280281bd649021e9423db55a589cb8945596a
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-11-27 11:47:42 +0100

    Added shadcn card and fixed it's styling

--- components/ui/PetCard.jsx
+++ components/ui/PetCard.jsx
@@ -1,9 +1,27 @@
 import animals from "@/public/pets.json" assert { type: 'json' };
+import Image from "next/image";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
 console.log(animals);
 
 function PetCard () {
+    const petPicture = animals.animals.dogs[0].picture;
+    const petName = animals.animals.dogs[0].name;
     return (
-    <h1>This is a pet card page</h1>
+        <>
+        <Card>
+            <Image src={petPicture} alt="pet image" width={200} height={200} className="mt-5"/>
+            <CardHeader>
+                <CardTitle>{petName}</CardTitle>
+            </CardHeader>
+        </Card>
+        </>
     )
 }
 
