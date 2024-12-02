983f527cacbcd783693224886089cf160e4e4193
maria <delapazferreras.maria@gmail.com>
2024-12-02 15:44:00 +0100

    Create login page and implement use router in login and register

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,54 +1,50 @@
 'use client';
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-const RegisterPage = () => {
+const LoginPage = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const router = useRouter();
 
   const onSubmit = async (data) => { 
-    const { name, username, email, password } = data;
-
-    if (password !== confirmPassword) {
-      setErrorMessage("Passwords do not match.");
-      return;
-    }
+    const { username, password } = data;
 
     try {
-      const response = await fetch('http://localhost:3001/api/v1/register', {
+      const response = await fetch('http://localhost:3001/api/v1/login', {
         method: 'POST',
         "Access-Control-Allow-Origin": 'http://localhost:3001',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, username, email, password }),
+        body: JSON.stringify({ username, password }),
         credentials: 'include',
       });
 
       const result = await response.json();
 
       if (response.ok) {
-        setSuccessMessage(result.message || "Registration successful!");
+        setSuccessMessage(result.message || "You logged in!");
         setTimeout(() => router.push('/'), 1000);
         
         setErrorMessage(null)
       } else {
-        setErrorMessage(result.message || "Registration failed.");
+        setErrorMessage(result.message || "Something went wrong when trying to log in.");
       }
     } catch (err) {
-      setErrorMessage("An error occurred while trying to register.");
+      setErrorMessage("An error occurred while trying to log in.");
       console.log(err.message);
     }
   };
 
-  return (
-    <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-semibold mb-4 text-center">Register</h2>
+
+    return (
+        <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold mb-4 text-center">Welcome back</h2>
 
       {successMessage && (
         <div className="p-4 mb-4 bg-green-100 text-green-800 rounded">{successMessage}</div>
@@ -65,32 +61,11 @@ const RegisterPage = () => {
       }}>
 
         <div className="mb-4">
-          <Label htmlFor="name">Name</Label>
-          <Input
-            id="name"
-            name="name"
-            placeholder="Enter your name"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
           <Label htmlFor="username">Username</Label>
           <Input
             id="username"
             name="username"
             placeholder="Enter your username"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Enter your email"
             required
           />
         </div>
@@ -108,25 +83,13 @@ const RegisterPage = () => {
           />
         </div>
 
-        <div className="mb-4">
-          <Label htmlFor="confirmPassword">Confirm Password</Label>
-          <Input
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirm your password"
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </div>
-
-        <Button type="submit">
-          Register
+        <Button type="submit" className="text-center">
+          Login
         </Button>
+        <p className="text-xs text-center">Do not have an acount? Sign up <Link href="/register" className="font-semibold underline">here</Link></p>
       </form>
     </div>
-  );
-};
+    )
+}
 
-export default RegisterPage;
+export default LoginPage;
