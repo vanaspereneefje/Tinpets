0adfbce1076052825438526be6906d2ac33e4a24
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-12-02 11:41:19 +0100

    Add fixed slider to pet carousel

--- components/PetCarousel.jsx
+++ components/PetCarousel.jsx
@@ -7,6 +7,8 @@ import {
   } from "@/components/ui/carousel"
 import PetCard from "./PetCard";
 import animals from "@/public/pets.json";
+import { Slider } from "@/components/ui/slider";
+
 
 
 function PetCarousel () {
@@ -14,13 +16,23 @@ function PetCarousel () {
 //TODO add array of matched animals instead of this placeholder 
     const dogCards = animals.animals.dogs;
 
+//TODO add actual value of energy slider
+    const sliderValue = animals.animals.dogs[0].energy;
+    console.log(sliderValue);
+
     return (
         <>
-        <Carousel className="w-[250px] left-1/2 translate-x-[-100px]">
+        <Carousel className="w-[250px] left-1/2 translate-x-[-125px]">
           <CarouselContent>
               {dogCards.map((petCard, index) => (
               <CarouselItem key={index}>
                   <PetCard petCard={petCard} index={index} />
+                  <div className="w-[250px] flex justify-self-center m-[20px]">
+                    <Slider defaultValue={[sliderValue]} max={100} step={1} disabled></Slider>
+                  </div>
+                  <div className="w-[250px] flex justify-self-center m-[20px]">
+                    
+                  </div>
               </CarouselItem>
               ))}
           </CarouselContent>
