2763350e03cc0430f48d0be15dc2720680b8969d
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-12-02 12:17:55 +0100

    Add more data to pets.json and add them to pet carousel

--- components/PetCarousel.jsx
+++ components/PetCarousel.jsx
@@ -16,10 +16,6 @@ function PetCarousel () {
 //TODO add array of matched animals instead of this placeholder 
     const dogCards = animals.animals.dogs;
 
-//TODO add actual value of energy slider
-    const sliderValue = animals.animals.dogs[0].energy;
-    console.log(sliderValue);
-
     return (
         <>
         <Carousel className="w-[250px] left-1/2 translate-x-[-125px]">
@@ -28,10 +24,22 @@ function PetCarousel () {
               <CarouselItem key={index}>
                   <PetCard petCard={petCard} index={index} />
                   <div className="w-[250px] flex justify-self-center m-[20px]">
-                    <Slider defaultValue={[sliderValue]} max={100} step={1} disabled></Slider>
+                    <Slider defaultValue={[animals.animals.dogs[index].energy]} max={100} step={1} disabled></Slider>
                   </div>
-                  <div className="w-[250px] flex justify-self-center m-[20px]">
-                    
+                  <div className="w-[250px] flex justify-self-center m-[10px]">
+                    <p>age: {animals.animals.dogs[index].age}</p>
+                  </div>
+                  <div className="w-[250px] flex justify-self-center m-[10px]">
+                    <p>personality type: {animals.animals.dogs[index].personalityType}</p>
+                  </div>
+                  <div className="w-[250px] flex justify-self-center m-[10px]">
+                    <p>cat friendly: {animals.animals.dogs[index].catfriendly ? "yes" : "no"}</p>
+                  </div>
+                  <div className="w-[250px] flex justify-self-center m-[10px]">
+                    <p>dog friendly: {animals.animals.dogs[index].dogfriendly ? "yes" : "no"}</p>
+                  </div>
+                  <div className="w-[250px] flex justify-self-center m-[10px]">
+                    <p>kid friendly: {animals.animals.dogs[index].kidfriendly ? "yes" : "no"}</p>
                   </div>
               </CarouselItem>
               ))}
