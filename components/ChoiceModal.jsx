d9c3bd534eaca52f8669a9c6376480f52ad749d7
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-12-04 10:56:45 +0100

    Import font awesome icons and use router from next and  Initialize the router

---
+++ components/ChoiceModal.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+import { FaSearch, FaQuestionCircle } from "react-icons/fa"; 
+import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation'; 
+
+export const ChoiceModal = ({ onClose }) => {
+  const router = useRouter(); 
+
+}
