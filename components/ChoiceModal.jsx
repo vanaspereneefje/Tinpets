61a351046e521a6f9257fe1a304b1106ec9f37c7
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-12-04 11:02:37 +0100

    Navigate to the specified path, using Next.js router and use router to change the page

--- components/ChoiceModal.jsx
+++ components/ChoiceModal.jsx
@@ -4,8 +4,15 @@ import React from "react";
 import { FaSearch, FaQuestionCircle } from "react-icons/fa"; 
 import { Button } from "@/components/ui/button";
 import { useRouter } from 'next/navigation'; 
-
 export const ChoiceModal = ({ onClose }) => {
   const router = useRouter(); 
 
-}
+  // Navigate to the specified path, using Next.js router
+  const navigateTo = (path) => {
+    // console.log("Navigate to:", path);
+    onClose(); 
+    router.push(path); // Use router to change the page
+  };
+
+ 
+};
