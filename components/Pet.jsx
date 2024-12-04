fc6e3c932772c2b1208362ecb7fccafad206270e
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-12-04 12:25:13 +0100

    Added more styling to individual pet page

--- components/Pet.jsx
+++ components/Pet.jsx
@@ -8,14 +8,22 @@ function PetPageUI ({ petCard }) {
 
     return (
         <>
-        <div className="flex flex-col items-center text-center">
-            <Image src={petPicture} alt="pet image" width={200} height={200} className="mt-5"/>
-            <h1 className="text-3xl">{petName}</h1>
-            <p>age: {animals.animals.dogs[index].age}</p>
-            <p>personality type: {animals.animals.dogs[index].personalityType}</p>
-            <p>cat friendly: {animals.animals.dogs[index].catfriendly ? "yes" : "no"}</p>
-            <p>dog friendly: {animals.animals.dogs[index].dogfriendly ? "yes" : "no"}</p>
-            <p>kid friendly: {animals.animals.dogs[index].kidfriendly ? "yes" : "no"}</p>
+        <div className="relative left-1/2 mb-[100px] translate-x-[-200px]">
+            <div className="bg-orange-100 w-[450px] flex flex-col items-center rounded-xl">
+                <div className="flex flex-col items-center">
+                    <Image src={petPicture} alt="pet image" width={400} height={400} className="mt-5 rounded-xl"/>
+                </div>
+                <div className="text-center m-[20px]">
+                    <h1 className="text-3xl">{petName}</h1>
+                </div>
+                <div className="flex flex-col w-[400px] gap-[20px] mb-[20px]">
+                    <p><strong>age:</strong> {animals.animals.dogs[index].age}</p>
+                    <p><strong>personality type:</strong> {animals.animals.dogs[index].personalityType}</p>
+                    <p><strong>cat friendly:</strong> {animals.animals.dogs[index].catfriendly ? "yes" : "no"}</p>
+                    <p><strong>dog friendly:</strong> {animals.animals.dogs[index].dogfriendly ? "yes" : "no"}</p>
+                    <p><strong>kid friendly:</strong> {animals.animals.dogs[index].kidfriendly ? "yes" : "no"}</p>
+                </div>
+            </div>
         </div>
         </>
     )
