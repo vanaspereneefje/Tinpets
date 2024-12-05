df7b2f9279fd64adedb5e831591502279848455e
maria <delapazferreras.maria@gmail.com>
2024-12-05 16:06:45 +0100

    Implement backend to retrieve pet data

--- app/our-pets/page.jsx
+++ app/our-pets/page.jsx
@@ -1,33 +1,82 @@
 import PetCard from "@/components/PetCard";
-import animals from "@/public/pets.json";
+import { headers } from "next/headers";
 
-function OurPetsPage () {
-    const dogCards = animals.animals.dogs;
-    const catCards = animals.animals.cats;
-    const birdCards = animals.animals.birds;
+export default async function OurPetsPage() {
+
+    async function getCards() {
+        const headersList = await headers(); 
+        
+        try {
+    
+            const response = await fetch('http://localhost:3001/api/v1/pets/get-all', {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    ...(headersList.get('cookie') ? { Cookie: headersList.get('cookie') } : {}),
+                },
+            });
+
+            if (!response.ok) {
+                console.log('Failed to fetch pets data');
+            }
+    
+            const data = await response.json();
+            console.log(data)
+    
+            return data;
+        } catch (error) {
+            console.log('Error fetching pets data:', error);
+            return "Error fetching pets data."
+        }
+    }
+
+    const petsData = await getCards();
+
+    const dogCards = petsData
+        .filter(pet => pet.species === "Dog")
+        .map(pet => (
+            <PetCard key={pet.id} petCard={pet} />
+        ));
+
+    const catCards = petsData
+        .filter(pet => pet.species === "Cat")
+        .map(pet => (
+            <PetCard key={pet.id} petCard={pet} />
+        ));
+
+    const birdCards = petsData
+        .filter(pet => pet.species === "Bird")
+        .map(pet => (
+            <PetCard key={pet.id} petCard={pet} />
+        ));
+
+    const fishCards = petsData
+        .filter(pet => pet.species === "Fish")
+        .map(pet => (
+            <PetCard key={pet.id} petCard={pet} />
+        ));
+
     return (
         <>
             <h1 className='font-bold text-2xl text-center mb-5 mt-10'>Dogs</h1>
             <div className="flex flex-wrap justify-center gap-5">
-                {dogCards.map((petCard, index) => (
-                    <PetCard key={index} petCard={petCard} index={index}/>
-                ))}
+                {dogCards}
             </div>
+
             <h1 className='font-bold text-2xl text-center mb-5 mt-10'>Cats</h1>
             <div className="flex flex-wrap justify-center gap-5">
-            {catCards.map((petCard, index) => (
-                <PetCard key={index} petCard={petCard} index={index}/>
-            ))}
+                {catCards}
             </div>
+
             <h1 className='font-bold text-2xl text-center mb-5 mt-10'>Birds</h1>
             <div className="flex flex-wrap justify-center gap-5">
-            {birdCards.map((petCard, index) => (
-                <PetCard key={index} petCard={petCard} index={index}/>
-            ))}
+                {birdCards}
+            </div>
+
+            <h1 className='font-bold text-2xl text-center mb-5 mt-10'>Fishes</h1>
+            <div className="flex flex-wrap justify-center gap-5">
+                {fishCards}
             </div>
         </>
-
-    )
+    );
 }
-
-export default OurPetsPage;
